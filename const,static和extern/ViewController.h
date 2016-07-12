04f44eccbd50c1638048eb0bb19be15ec7994271
zhangyuhao <zhangyuhao1158@sina.com>
2016-07-12 13:55:56 +0800

    Initial Commit

---
+++ const,static和extern/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  const,static和extern
+//
+//  Created by zyh on 16/7/12.
+//  Copyright © 2016年 zyh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ const,static和externTests/const_static_externTests.m
@@ -0,0 +1,39 @@
+//
+//  const_static_externTests.m
+//  const,static和externTests
+//
+//  Created by zyh on 16/7/12.
+//  Copyright © 2016年 zyh. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface const_static_externTests : XCTestCase
+
+@end
+
+@implementation const_static_externTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
